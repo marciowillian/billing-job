57f9ed35b90f44dcf133e84a60ef3e5ae4a1c9f0
Marcio Willian <willian_mw.cc@hotmail.com>
2024-03-03 02:47:28 -0300

    Refactoring a test to clean metadatas

--- src/test/java/com/mwcc/billingjob/BillingJobApplicationTests.java
+++ src/test/java/com/mwcc/billingjob/BillingJobApplicationTests.java
@@ -1,38 +1,51 @@
 package com.mwcc.billingjob;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
+@SpringBatchTest
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
 class BillingJobApplicationTests {
 
 	@Autowired
-	private Job job;
+	private JobLauncherTestUtils jobLauncherTestUtils;
 	
 	@Autowired
-	private JobLauncher jobLauncher;
+	private JobRepositoryTestUtils jobRepositoryTestUtils;
+	
+//	@Autowired
+//	private Job job;
+//	
+//	@Autowired
+//	private JobLauncher jobLauncher;
+	
+	@BeforeEach
+	public void setUp() {
+		this.jobRepositoryTestUtils.removeJobExecutions();
+	}
 	
 	@Test
 	void testJobExecution(CapturedOutput output) throws Exception {
 		//given
-		JobParameters jobParameters = new JobParametersBuilder()
-				.addString("input.file", "/some/input/file")
+		JobParameters jobParameters = this.jobLauncherTestUtils.getUniqueJobParametersBuilder()
+				.addString("input.file", "/some/imput/file")
 				.toJobParameters();
-		
+						
 		//when
-		JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
+		JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
 		
 		//then
 		Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
